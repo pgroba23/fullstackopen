d5d55d2db8f094735c1eebbf0e77890856fe5f7b
Pablo Groba <pgroba@gmail.com>
2025-11-03 17:29:46 +0100

    adding changes

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,9 +6,9 @@ const Part = ({part, exercises}) =>{
 }
 const Content = ({parts}) =>{
   return(
-    <p>
+    <>
       {parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises} />)}
-    </p>
+    </>
   )
 }
 const Total = ({parts}) =>{
@@ -18,30 +18,32 @@ const Total = ({parts}) =>{
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      id: 1,
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      id: 2,
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      id: 3,
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        id: 1,
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        id: 2,
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        id: 3,
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
